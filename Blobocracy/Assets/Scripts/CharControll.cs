1eb0fc8bacc93d8d12a9019d25ffd6e7eb88dd6b
Zoolouie <zander@bugsplat.com>
2020-02-08 00:42:22 -0700

    Added jumping

--- Blobocracy/Assets/Scripts/CharControll.cs
+++ Blobocracy/Assets/Scripts/CharControll.cs
@@ -9,6 +9,8 @@ public class CharControll : MonoBehaviour
     protected Rigidbody2D body;
     protected Vector2 velocity;
 
+    bool isGrounded;
+
     
     [SerializeField, Tooltip("Acceleration while grounded.")]
     float acceleration = 75;
@@ -26,12 +28,10 @@ public class CharControll : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float moveInput = Input.GetAxis("Horizontal");
-
         //Lets do some jumping 
-        velocity.y = 0;
 
         if (moveInput != 0)
         {
@@ -43,15 +43,18 @@ public class CharControll : MonoBehaviour
         }   
 
         transform.Translate(velocity * Time.deltaTime);
-                if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Debug.Log("tset");
             body.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            isGrounded = false;
         }
-    }
-
-    void FixedUpdated() {
-        
-
+    }  
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("Test");
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.name == "Floor")
+        {
+            isGrounded = true;
+        }
     }
 }
