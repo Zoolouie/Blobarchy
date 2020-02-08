0336b4ee252d103befcf24a0bf5078a148c44ffb
Zoolouie <zander@bugsplat.com>
2020-02-07 22:05:56 -0700

    Made our boy move fast

--- Blobocracy/Assets/Scripts/CharControll.cs
+++ Blobocracy/Assets/Scripts/CharControll.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CharControll : MonoBehaviour
 {
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
     protected Rigidbody2D body;
     protected Vector2 velocity;
+
+    
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float acceleration = 75;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float deceleration = 70;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +25,17 @@ public class CharControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontal, 0.0f, 0.0f);
-        // float vertical = Input.GetAxis("Vertical") * speed;
-        transform.Translate(movement * Time.deltaTime);
+        float moveInput = Input.GetAxis("Horizontal");
+
+        if (moveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        transform.Translate(velocity * Time.deltaTime);
     }
 }
