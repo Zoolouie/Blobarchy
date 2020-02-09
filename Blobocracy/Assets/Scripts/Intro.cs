24b3cde6acd86a56dda32fd2950570c8fda15b64
Zoolouie <zander@bugsplat.com>
2020-02-08 17:45:26 -0700

    Added intro screen

--- Blobocracy/Assets/Scripts/Intro.cs
+++ Blobocracy/Assets/Scripts/Intro.cs
@@ -2,26 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
 
-    float timer = 5.0f;
+    public Sprite logo;
+    float timer;
+    float timer2;
+    Image image;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = GetComponent<Image>();
+        image.canvasRenderer.SetAlpha(0.0f);
+        fadeIn();
+        timer = 3f;
+        timer2 = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;
+        timer2 -= Time.deltaTime;
+
+        if(timer2 <= 0) {
+            SwitchScenes();
+        }
 
         if(timer <= 0) {
-            
+            SwitchImages();
+            fadeOut();
         }
+    }
+
+    void SwitchImages() {
+        image.sprite = logo;
+    }
+
+    void fadeOut() {
+        image.CrossFadeAlpha(0, 2, false);
+    }
+    void fadeIn() {
+        image.CrossFadeAlpha(1, 2, false);
+    }
+
+    void SwitchScenes() {
+        SceneManager.LoadScene("Intro");
     }
 }
